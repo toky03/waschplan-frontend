e59177a478d160d427f255b4ba6bc5b52a4754a2
MichaelEgli <michaelegli1879@gmail.com>
2021-01-30 12:35:51 +0100

    add nav links

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import { Route } from 'react-router';
 import Termine from './Termine'
 import TerminInput from '../containers/ErfasseTermin'
 
 const App = () => (
   <Router>
+    <ul>
+      <li>
+        <NavLink to="/">Waschplan</NavLink>
+      </li>
+      <li>
+        <NavLink to="/verwalten">Verwalten</NavLink>
+      </li>
+    </ul>
     <div>
       <Route path="/" exact component={TerminInput}>
       </Route>
