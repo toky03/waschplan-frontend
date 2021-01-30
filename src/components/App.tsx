94c01f2c38d662dbbee56ea94b356ac59dc541ae
MichaelEgli <michaelegli1879@gmail.com>
2021-01-30 12:26:00 +0100

    add path verwalten

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router';
 import Termine from './Termine'
 import TerminInput from '../containers/ErfasseTermin'
 
 const App = () => (
-  <div>
-    <h1>Termine</h1>
-    <Termine />
-    <div>Name und Datum eingeben</div>
-    <TerminInput />  
-  </div>
+  <Router>
+    <div>
+      <Route path="/" exact component={TerminInput}>
+      </Route>
+      <Route path="/verwalten" exact render={ () => {
+        return(<h1>Welcome Verwalten</h1>)
+          }
+        }>
+      </Route>
+    </div>
+</Router>
 )
 
 export default App;
+
+
+/* 
+    <h1>Termine</h1>
+    <Termine />
+    <div>Name und Datum eingeben</div>
+    <TerminInput /> 
+*/
