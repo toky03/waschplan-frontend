b6c2ca73e10128f911acef9be81eb51001ce3322
MichaelEgli <michaelegli1879@gmail.com>
2021-02-17 11:39:32 +0100

    store any damit mergbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import combineReducers from './reducers';
 import { Provider } from 'react-redux';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from 'redux-thunk';
-import {​​​​​ CombinedState }​​​​​ from '@reduxjs/toolkit';
 
-let store: CombinedState<any>;
+let store: any;
 
 if(process.env.NODE_ENV === 'development'){
     store = createStore(combineReducers, composeWithDevTools(applyMiddleware(thunk)));
