aa728f7c651e9b9a4a2f8cdfa46885c525b81bc7
Marco <marco.jakob3@gmail.com>
2021-03-11 16:14:47 +0100

    Kleine Korrekturen Marcfo

--- src/components/Termine.tsx
+++ src/components/Termine.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import React, {useEffect, useState} from "react";
+import {makeStyles, useTheme, Theme, createStyles} from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -13,11 +13,13 @@ import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
-import {Termin} from "../model/model";
-import { useSelector } from "react-redux";
-import { selectTermineEnriched } from "../state/selectors";
+import {Termin, TerminDto} from "../model/model";
+import {useSelector} from "react-redux";
+import {selectTermineEnriched} from "../state/selectors";
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
+import {TableHead} from "@material-ui/core";
+import {format, parseISO} from "date-fns";
 
 const useStyles1 = makeStyles((theme: Theme) =>
     createStyles({
@@ -38,7 +40,7 @@ interface TablePaginationActionsProps {
 function TablePaginationActions(props: TablePaginationActionsProps) {
     const classes = useStyles1();
     const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
+    const {count, page, rowsPerPage, onChangePage} = props;
 
     const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         onChangePage(event, 0);
@@ -63,125 +65,120 @@ function TablePaginationActions(props: TablePaginationActionsProps) {
                 disabled={page === 0}
                 aria-label="first page"
             >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
             </IconButton>
             <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
             </IconButton>
             <IconButton
                 onClick={handleNextButtonClick}
                 disabled={page >= Math.ceil(count / rowsPerPage) - 1}
                 aria-label="next page"
             >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
             </IconButton>
             <IconButton
                 onClick={handleLastPageButtonClick}
                 disabled={page >= Math.ceil(count / rowsPerPage) - 1}
                 aria-label="last page"
             >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
             </IconButton>
         </div>
     );
 }
 
-function createData(name: string | undefined, start: string, ende: string) {
-    return { name, start, ende };
+interface TerminRow {
+    id: string,
+    name: string,
+    start: string;
+    ende: string;
 }
 
-// const rows = [
-//     createData('Cupcake', 305, 3.7),
-//     createData('Donut', 452, 25.0),
-//     createData('Eclair', 262, 16.0),
-//     createData('Frozen yoghurt', 159, 6.0),
-//     createData('Gingerbread', 356, 16.0),
-//     createData('Honeycomb', 408, 3.2),
-//     createData('Ice cream sandwich', 237, 9.0),
-//     createData('Jelly Bean', 375, 0.0),
-//     createData('KitKat', 518, 26.0),
-//     createData('Lollipop', 392, 0.2),
-//     createData('Marshmallow', 318, 0),
-//     createData('Nougat', 360, 19.0),
-//     createData('Oreo', 437, 18.0),
-// ].sort((a, b) => (a.calories < b.calories ? -1 : 1));
+function createData(termin: Termin): TerminRow {
+    return {
+        id: termin.id,
+        name: termin.mieterName,
+        start: prettyPrintDate(termin.terminBeginn),
+        ende: prettyPrintDate(termin.terminEnde)
+    };
+}
 
-const useStyles2 = makeStyles({
-    table: {
-        minWidth: 500,
-    },
-});
+function prettyPrintDate(date: string): string {
+    return format(parseISO(date), "dd.MM.yyyy HH:mm")
+}
 
 const Termine = () => {
     const termine: Termin[] | undefined = useSelector(selectTermineEnriched);
+    const columnHeader = ["Mietername", "Beginn", "Ende", "Editieren"]
 
+    const terminRows = termine.map((termin: Termin) => createData(termin));
 
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
-    const rows = termine.map(termin => (
-        createData(termin.mieterName, termin.terminBeginn, termin.terminEnde)
-    ));
-
-    const classes = useStyles2();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, terminRows.length - page * rowsPerPage);
 
     const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-        setPage(newPage);
+        setPage(() => newPage);
     };
 
     const handleChangeRowsPerPage = (
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     ) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
+        setRowsPerPage(() => parseInt(event.target.value, 10));
+        setPage(() => 0);
     };
 
     return (
         <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="custom pagination table">
+            <Table aria-label="custom pagination table">
+                <TableHead>
+                    <TableRow>
+                        {columnHeader.map((column: string) => (
+                            <TableCell key={column} align={"center"}>{column}</TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
                 <TableBody>
-                    {(rowsPerPage > 0
-                            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                            : rows
+                    {(terminRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     ).map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell style={{ width: 160 }} component="th" scope="row" align="center">
+                        <TableRow key={row.id}>
+                            <TableCell style={{width: 160}} component="th" scope="row" align="center">
                                 {row.name}
                             </TableCell>
-                            <TableCell style={{ width: 160 }} align="center">
+                            <TableCell style={{width: 160}} align="center">
                                 {row.start}
                             </TableCell>
-                            <TableCell style={{ width: 160 }} align="center">
+                            <TableCell style={{width: 160}} align="center">
                                 {row.ende}
                             </TableCell>
-                            <TableCell style={{ width: 160 }} align="center">
+                            <TableCell style={{width: 160}} align="center">
                                 <IconButton aria-label="delete" color="secondary">
-                                    <DeleteIcon />
+                                    <DeleteIcon/>
                                 </IconButton>
                                 <IconButton aria-label="edit">
-                                    <EditIcon />
+                                    <EditIcon/>
                                 </IconButton>
                             </TableCell>
                         </TableRow>
                     ))}
                     {emptyRows > 0 && (
-                        <TableRow style={{ height: 53 * emptyRows }}>
-                            <TableCell colSpan={6} />
+                        <TableRow style={{height: 53 * emptyRows}}>
+                            <TableCell colSpan={6}/>
                         </TableRow>
                     )}
                 </TableBody>
                 <TableFooter>
                     <TableRow>
                         <TablePagination
-                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            rowsPerPageOptions={[5, 10, 25, {label: 'All', value: -1}]}
                             colSpan={3}
-                            count={rows.length}
+                            count={terminRows.length}
                             rowsPerPage={rowsPerPage}
                             page={page}
                             SelectProps={{
-                                inputProps: { 'aria-label': 'rows per page' },
+                                inputProps: {'aria-label': 'rows per page'},
                                 native: true,
                             }}
                             onChangePage={handleChangePage}
@@ -194,37 +191,6 @@ const Termine = () => {
         </TableContainer>
     );
 
-    // return (
-    //     <TableContainer component={Paper}>
-    //       <Table aria-label="simple table">
-    //         <TableHead>
-    //           <TableRow>
-    //             <TableCell align="center">Familienname</TableCell>
-    //             <TableCell align="center">Beginn</TableCell>
-    //             <TableCell align="center">Ende</TableCell>
-    //               <TableCell align="center"></TableCell>
-    //           </TableRow>
-    //         </TableHead>
-    //         <TableBody>
-    //           {termine?.map((termin: Termin) => (
-    //               <TableRow key={termin.id}>
-    //                 <TableCell align="center">{termin.mieterName}</TableCell>
-    //                 <TableCell align="center">{termin.terminBeginn}</TableCell>
-    //                 <TableCell align="center">{termin.terminEnde}</TableCell>
-    //                 <TableCell align="center">
-    //                     <IconButton aria-label="delete">
-    //                       <DeleteIcon />
-    //                     </IconButton>
-    //                     <IconButton aria-label="edit">
-    //                         <EditIcon />
-    //                     </IconButton>
-    //                 </TableCell>
-    //               </TableRow>
-    //           ))}
-    //         </TableBody>
-    //       </Table>
-    //     </TableContainer>
-    // );
 };
 
 export default Termine;
