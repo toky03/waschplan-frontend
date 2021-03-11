b8478ab9f3b97aa409661d7398b7935e04b7d649
MichaelEgli <michaelegli1879@gmail.com>
2021-03-11 11:42:05 +0100

    backend path revert

--- src/integration/backend.ts
+++ src/integration/backend.ts
@@ -1,8 +1,8 @@
 import { MieterDto, ReplacedIdDto, TerminDto } from "../model/model";
 import axios from "axios";
 
-// const API_URL = "https://waschplan.bubelu.ch/api/";
-const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://waschplan.bubelu.ch/api/";
+//const API_URL = "http://localhost:8080/api/";
 const TIMEOUT_MILLIS = 5000;
 
 export async function available(): Promise<boolean> {
