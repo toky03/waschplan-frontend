962dc0b6b45bdfe636842d90dd21d1abc5bb02dd
MichaelEgli <michaelegli1879@gmail.com>
2021-03-14 15:43:06 +0100

    add deleteButton and function

--- src/components/Termine.tsx
+++ src/components/Termine.tsx
@@ -5,12 +5,17 @@ import { Termin } from "../model/model";
 
 const Termine = () => {
   const termine: Termin[] | undefined = useSelector(selectTermineEnriched);
+  
+  function deleteTermin() {
+    confirm("Willst Du den Termin wirklich löschen?")
+  }
   return (
     <ul>
       {termine?.map((termin: Termin) => (
         <li key={termin.id}>
           TerminId: {termin.id} Mieterpartei: {termin.mieterName} Beginn:{" "}
           {termin.terminBeginn} Ende: {termin.terminEnde}
+          <button onClick={deleteTermin}>Löschen</button>
         </li>
       ))}
     </ul>
