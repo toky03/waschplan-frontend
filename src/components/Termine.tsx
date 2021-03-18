cd29b5f3184be5a1f70911a0f9382cfb1d49a47a
RAT <remzi.atesci@students.bfh.ch>
2021-03-18 19:32:57 +0100

    added box-shadow for Kalender and new Table with pagination, sorting and delete icon for TermineVerwalten

--- src/components/Termine.tsx
+++ src/components/Termine.tsx
@@ -1,118 +1,38 @@
-import React, {useState} from "react";
-import {createStyles, makeStyles, Theme, useTheme} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import React from "react";
 import {Termin} from "../model/model";
 import {useSelector} from "react-redux";
 import {selectTermineEnriched} from "../state/selectors";
 import DeleteIcon from '@material-ui/icons/Delete';
-import {TableHead} from "@material-ui/core";
 import {format, parseISO} from "date-fns";
 import store from "../index";
 import {deleteTermin} from "../integration/integration";
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexShrink: 0,
-            marginLeft: theme.spacing(2.5),
-        },
-        table: {
-            marginTop: '50px',
-            marginBottom: 'auto',
-            marginInline: 'auto',
-            width: "80%",
-            backgroundColor: '#edcfb7'
-        },
-    }),
-);
-
-interface TablePaginationActionsProps {
-    count: number;
-    page: number;
-    rowsPerPage: number;
-    onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
-}
-
-function TablePaginationActions(props: TablePaginationActionsProps) {
-    const classes = useStyles();
-    const theme = useTheme();
-    const {count, page, rowsPerPage, onChangePage} = props;
-
-    const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
-            </IconButton>
-        </div>
-    );
-}
+import {createStyles, makeStyles} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Paper from '@material-ui/core/Paper';
+import {IconButton} from "@material-ui/core";
 
 interface TerminRow {
     id: string,
     name: any,
     start: string;
     ende: string;
+    delete: any;
 }
-
-const removeTermin = (id: string) => {
-    store.dispatch(deleteTermin(id));
-};
 
 function createData(termin: Termin): TerminRow {
     return {
         id: termin.id,
         name: termin.mieterName,
         start: prettyPrintDate(termin.terminBeginn),
-        ende: prettyPrintDate(termin.terminEnde)
+        ende: prettyPrintDate(termin.terminEnde),
+        delete: termin.mieterName,
     };
 }
 
@@ -120,91 +40,242 @@ function prettyPrintDate(date: string): string {
     return format(parseISO(date), "dd.MM.yyyy HH:mm")
 }
 
-const Termine = () => {
-    const classes = useStyles();
 
-    const termine: Termin[] | undefined = useSelector(selectTermineEnriched);
-    const columnHeader = ["Mietername", "Beginn", "Ende", "Löschen / Editieren"]
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
 
-    const terminRows = termine!.map((termin: Termin) => createData(termin));
+type Order = 'asc' | 'desc';
 
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+function getComparator<Key extends keyof any>(
+    order: Order,
+    orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
 
-    const emptyRows = rowsPerPage - Math.min(rowsPerPage, terminRows.length - page * rowsPerPage);
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) return order;
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
 
-    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-        setPage(() => newPage);
-    };
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof TerminRow;
+    label: string;
+    numeric: boolean;
+}
 
-    const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    ) => {
-        setRowsPerPage(() => parseInt(event.target.value, 10));
-        setPage(() => 0);
+const headCells: HeadCell[] = [
+    {id: 'name', numeric: false, disablePadding: true, label: 'Mietername'},
+    {id: 'start', numeric: true, disablePadding: false, label: 'Beginn'},
+    {id: 'ende', numeric: true, disablePadding: false, label: 'Ende'},
+    {id: 'delete', numeric: true, disablePadding: false, label: 'Löschen'}
+];
+
+interface EnhancedTableProps {
+    classes: ReturnType<typeof useStyles>;
+    numSelected: number;
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof TerminRow) => void;
+    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const {classes, order, orderBy, onRequestSort} = props;
+    const createSortHandler = (property: keyof TerminRow) => (event: React.MouseEvent<unknown>) => {
+        onRequestSort(event, property);
     };
 
     return (
-        <TableContainer className={classes.table} component={Paper} elevation={6}>
-            <Table aria-label="custom pagination table">
-                <TableHead>
-                    <TableRow>
-                        {columnHeader.map((column: string) => (
-                            <TableCell key={column} align={"center"}>{column}</TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {(terminRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                    ).map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell style={{width: 160}} component="th" scope="row" align="center">
-                                {row.name}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="center">
-                                {row.start}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="center">
-                                {row.ende}
-                            </TableCell>
-                            <TableCell style={{width: 160}} align="center">
-                                <IconButton onClick={() => removeTermin(row.id)} aria-label="delete" color="secondary">
-                                    <DeleteIcon/>
-                                </IconButton>
-                                {/*                                <IconButton aria-label="edit">
-                                    <EditIcon/>
-                                </IconButton>*/}
-                                {/*TODO: edit funktion kann hier hinzugefügt werden!*/}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                    {emptyRows > 0 && (
-                        <TableRow style={{height: 53 * emptyRows}}>
-                            <TableCell colSpan={6}/>
-                        </TableRow>
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10]}
-                            colSpan={3}
-                            count={terminRows.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            SelectProps={{
-                                inputProps: {'aria-label': 'rows per page'},
-                                native: true,
-                            }}
-                            onChangePage={handleChangePage}
-                            onChangeRowsPerPage={handleChangeRowsPerPage}
-                            ActionsComponent={TablePaginationActions}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox"/>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <span className={classes.visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </span>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
     );
-};
+}
 
-export default Termine;
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            width: '100%',
+        },
+        paper: {
+            marginTop: '50px',
+            marginBottom: 'auto',
+            marginInline: 'auto',
+            width: "80%",
+            backgroundColor: '#edcfb7'
+        },
+        table: {
+            minWidth: 750,
+        },
+        visuallyHidden: {
+            border: 0,
+            clip: 'rect(0 0 0 0)',
+            height: 1,
+            margin: -1,
+            overflow: 'hidden',
+            padding: 0,
+            position: 'absolute',
+            top: 20,
+            width: 1,
+        },
+    }),
+);
+
+
+export default function EnhancedTable() {
+    const classes = useStyles();
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof TerminRow>('name');
+    const [selected, setSelected] = React.useState<string[]>([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const termine: Termin[] | undefined = useSelector(selectTermineEnriched);
+    const terminRows = termine!.map((termin: Termin) => createData(termin));
+
+    const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof TerminRow) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+            const newSelecteds = terminRows.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const isSelected = (name: number | string) => selected.indexOf(name as string) !== -1;
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, terminRows.length - page * rowsPerPage);
+
+    const removeTermin = (id: number | string) => {
+        store.dispatch(deleteTermin(id as string));
+    };
+
+    return (
+        <div className={classes.root}>
+            <Paper className={classes.paper} elevation={6}>
+                <TableContainer>
+                    <Table
+                        className={classes.table}
+                        aria-labelledby="tableTitle"
+                        aria-label="enhanced table"
+                    >
+                        <EnhancedTableHead
+                            classes={classes}
+                            numSelected={selected.length}
+                            order={order}
+                            orderBy={orderBy}
+                            onSelectAllClick={handleSelectAllClick}
+                            onRequestSort={handleRequestSort}
+                            rowCount={terminRows.length}
+                        />
+                        <TableBody>
+                            {stableSort(terminRows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row, index) => {
+                                    const isItemSelected = isSelected(row.name);
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow
+                                            hover
+                                            role="checkbox"
+                                            aria-checked={isItemSelected}
+                                            tabIndex={-1}
+                                            key={row.name}
+                                            selected={isItemSelected}
+                                        >
+                                            <TableCell padding="checkbox">
+
+                                            </TableCell>
+                                            <TableCell component="th" id={labelId} scope="row" padding="none">
+                                                {row.name}
+                                            </TableCell>
+                                            <TableCell align="right">{row.start}</TableCell>
+                                            <TableCell align="right">{row.ende}</TableCell>
+                                            <TableCell align="right">
+                                                <IconButton onClick={() => removeTermin(row.id)} aria-label="delete"
+                                                            color="secondary">
+                                                    <DeleteIcon/>
+                                                </IconButton>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow>
+                                    <TableCell colSpan={6}/>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10]}
+                    component="div"
+                    count={terminRows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </div>
+    );
+}
